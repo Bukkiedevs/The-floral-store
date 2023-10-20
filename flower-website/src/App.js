bec9d6a5e46007ad4750cf9d12de598d9de99d54
Bukkiedevs <bukkie92@gmail.com>
2023-10-21 01:11:00 +0200

    First push

---
+++ flower-website/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./components/nav";
+import { BrowserRouter } from "react-router-dom";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ flower-website/src/components/nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaShippingFast } from "react-icons/fa";
+import { GrSearch } from "react-icons/gr";
+import "./nav.css";
+const Nav = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="top-header">
+          <div className="icon">
+            <FaShippingFast />
+          </div>
+          <div className="info">
+            <p>Next day delivery when you shop up to SEK 500</p>
+          </div>
+        </div>
+        <div className="mid-header">
+          <div className="logo">
+            <img src="images/floral.logo.png" alt="logo"></img>
+          </div>
+          <div className="search-box">
+            <input type="text" value="" placeholder="Search"></input>
+            <button>
+              {" "}
+              <GrSearch />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Nav;
